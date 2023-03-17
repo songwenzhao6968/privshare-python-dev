81c2175f15f67a1c40de44b43f48066d62092381
Songwen Zhao <songwenzhao6968@gmail.com>
2023-03-17 09:38:36 +0000

    update myutil

--- myutil.py
+++ myutil.py
@@ -4,7 +4,7 @@ def int_to_uint(x, bit_width):
     return x + 2**(bit_width-1)
   
 def str_to_uint(str):
-    # SHA-256 hashing, then use "xor" to reduce to 32 bits
+    # SHA-256 hashing, then use xor to reduce to 32 bits
     result = hashlib.sha256(str.encode()).hexdigest()
     ret = 0
     for i in range(0, 64, 8):
