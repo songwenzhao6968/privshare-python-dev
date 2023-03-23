de1ab28ec2f03ed49cc9889a44f8a3581dc999e5
Songwen Zhao <songwenzhao6968@gmail.com>
2023-03-23 04:19:30 +0000

    don: return indicator vector

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
-import sys
-import os
 import json
 from database import DataBase, Schema
 from sql_parser import Query
 from secure_query import SecureQuery
+import numpy as np
 import he
 
 def run_secure_query(db: DataBase, secure_query: SecureQuery):
     pass
 
-example_dir = "./examples/demo"
-with open(os.path.join(example_dir, "config.json")) as f:
+config_dir = "./examples/demo/config.json"
+with open(config_dir) as f:
     config = json.load(f)
+debug = config["debug"]
 
-# Client View
-sql = ""
-schema = Schema() # ?
+# Client: Encrypt and send the query to the data provider
+sql = "SELECT SUM(deposit) FROM t_deposit WHERE user_name = \"Robert\""
 query = Query(sql)
+with open(config["tables"][query.concerned_table]["schema_file_loc"]) as f:
+    null_data_db = DataBase.deserialize_from_json(json.load(f))
+    schema = null_data_db[query.concerned_table].schema
 
 HE = he.create_he_object(config["he"])
-secure_query = SecureQuery(query, schema, HE, config["execution"])
+secure_query = SecureQuery(query, schema, HE, config["compile"])
 pub_keys_bytes = he.save_public_to_bytes(HE)
-secure_query_dump, ciphers_bytes = secure_query.dumps()
+secure_query_dump, ciphers_bytes = secure_query.dump()
 
-# Data Provider View
-db = DataBase(config["servers"]["provider_1"]["database_file_loc"])
+if debug["print_secure_execution_tree"]:
+    print(secure_query_dump)
+
+# Data Provider: Process encrypted query
+with open(config["servers"]["provider_1"]["database_file_loc"]) as f:
+    db = DataBase.deserialize_from_json(json.load(f))
 
 HE_pub = he.load_public_from_bytes(pub_keys_bytes)
 secure_query = SecureQuery.from_dump(secure_query_dump, ciphers_bytes, HE_pub)
-run_secure_query(db, secure_query)
+secure_result = secure_query.process(db, HE, None)
+
+# Client: Receive encrypted query result and decrypt
+ind = HE.decryptInt(secure_result[0])
+print("Result indicator vector:", ind.nonzero())
